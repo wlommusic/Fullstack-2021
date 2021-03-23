f43e7d06423ba37a9ba58daeea88987a45381512
Dipta ranjan panda <64889320+wlommusic@users.noreply.github.com>
2021-03-23 14:23:56 +0530

    part 1

---
+++ part 1/courseinfo/src/App.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+
+const Header =(props)=>{
+  return(
+    <>
+    <h1>{props.course.name}</h1>
+    </>
+  )
+}
+const Part =(props)=>{
+  return(
+    <>
+    <p>{props.part} {props.exercises}</p>
+    </>
+  )
+}
+const Content = (props)=>{
+  return(
+    <>
+    <Part part={props.course.part[0].name} exercises={props.course.part[0].exercises}/>
+    <Part part={props.course.part[1].name} exercises={props.course.part[1].exercises}/>
+    <Part part={props.course.part[2].name} exercises={props.course.part[2].exercises}/>
+    </>
+  )
+}
+const Total =(props)=>{
+  return (
+    <>
+      <p>
+        Number of exercises{" "}
+        {props.course.part[0].exercises +
+          props.course.part[1].exercises +
+          props.course.part[2].exercises}
+      </p>
+    </>
+  );
+}
+const App = () => {
+ const course = {
+        name: 'Half Stack application development',
+        part:[
+            {
+            name: 'Fundamentals of React',
+            exercises: 10
+            },
+            {
+            name: 'Using props to pass data',
+            exercises: 7
+            },
+            {
+            name: 'State of a component',
+            exercises: 14
+            }
+       ]
+    }
+  return (
+    <div>
+      <Header course={course}/>
+      <Content course={course}/>
+      <Total course={course}/>
+    </div>
+  );
+};
+
+export default App;
